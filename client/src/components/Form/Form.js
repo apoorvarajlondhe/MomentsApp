47480fdbc738e12746c50be9570ebeddf8918469
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2023-08-09 17:03:02 +0530

    added cards to pages

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -46,8 +46,11 @@ const Form = () => {
 
     return(
         <Paper className={classes.paper}>
+            
+
+
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a moment</Typography>
+            <Typography variant="h6">Creating a Moment</Typography>
             <TextField name="creator" variant="outlined" label="Creator"  fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value})}/>
             <TextField name="title" variant="outlined" label="Title"  fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})}/>
             <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value})}/>
@@ -69,6 +72,7 @@ const Form = () => {
             <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
             <Button  variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form> 
+            
             {/* <img width={350} height={200} src={postData.selectedFile}/> */}
         </Paper>
     );
